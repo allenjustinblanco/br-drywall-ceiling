f28eb3c5759d6a1cf1e072a351ac7eb45b40284b
shaun mccormack <shaunjmccormack@gmail.com>
2021-12-19 12:45:16 -0500

    updateserver.js

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
-const express = require('express');const path = require('path');const app = express();app.use(express.static(__dirname + '/dist/<brdrywall>'));app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/<brdrywall>/index.html'));});app.listen(process.env.PORT || 8080);
+const express = require('express');const path = require('path');const app = express();app.use(express.static(__dirname + '/dist/br-drywall-ceiling'));app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/br-drywall-ceiling/index.html'));});app.listen(process.env.PORT || 8080);
 
 // now we need to add a node and npm engine that Heroku will use to run 
